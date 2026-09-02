bdee3377bf05a60ccbc003cf66aed61cb1d9d47c
ujjvalrl <ujjvalraghavendral@gmail.com>
2026-09-02 10:23:09 +0530

    Auto update: 2026-09-02 10:23:09

---
+++ 3 Circuits/3.2 Sequential Logic/3.2.2 Counters/105.Counter6(4bit_BCD).v
@@ -0,0 +1,48 @@
+// Author: Ujjval Raghavendra L
+
+module top_module (
+    input clk,
+    input reset,   // Synchronous active-high reset
+    output [3:1] ena,
+    output [15:0] q);
+    
+    always@(posedge clk)
+        begin
+            if(reset)
+                q<=16'b0;
+            
+            else begin
+                
+               	 if(q[3:0] == 4'd9)
+             		q[3:0] <=4'd0;
+                else
+                    q[3:0] <= q[3:0] + 4'd1;
+                
+            if (ena[1]) begin
+                if (q[7:4] == 4'd9)
+                    q[7:4] <= 4'd0;
+                else
+                    q[7:4] <= q[7:4] + 4'd1;
+            end
+
+            if (ena[2]) begin
+                if (q[11:8] == 4'd9)
+                    q[11:8] <= 4'd0;
+                else
+                    q[11:8] <= q[11:8] + 4'd1;
+            end
+
+            if (ena[3]) begin
+                if (q[15:12] == 4'd9)
+                    q[15:12] <= 4'd0;
+                else
+                    q[15:12] <= q[15:12] + 4'd1;
+            end
+        
+        end
+
+    assign ena[1] = (q[3:0] == 4'd9);
+    assign ena[2] = (q[7:4] == 4'd9) && ena[1];
+    assign ena[3] = (q[11:8] == 4'd9) && ena[2];
+
+endmodule
